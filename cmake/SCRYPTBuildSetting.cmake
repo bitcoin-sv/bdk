4ea0e14cf75cfbdc6477d55e60889754e22b0dd3
Chi Thanh Nguyen <c.nguyen@nchain.com>
2020-01-20 10:57:51 +0000

    init cmake build system

---
+++ cmake/SCRYPTBuildSetting.cmake
@@ -0,0 +1,129 @@
+#################################################################
+#  Date             20/01/2020                                  #
+#  Author           Chi Thanh NGUYEN                            #
+#                                                               #
+#  Copyright (c) 2020 nChain Limited. All rights reserved       #
+#################################################################
+if(SCRYPTBuildSetting_Include)## Include guard
+  return()
+endif()
+set(SCRYPTBuildSetting_Include TRUE)
+
+macro(scryptSetCompilationOptions)## This has to be macro instead of a function because of add_definitions scope is local
+  if(NOT SCRYPT_SET_COMPILATION_OPTIONS_DONE)
+    ## Set common compile options
+    set(CMAKE_CXX_STANDARD 17 CACHE INTERNAL "Use of C++17" FORCE)#TODO : change to C++17 when visual studio support
+    set(CMAKE_CXX_STANDARD_REQUIRED ON CACHE INTERNAL "Require C++ standard" FORCE)
+    set(CMAKE_CXX_EXTENSIONS OFF CACHE INTERNAL "Do not use C++ extension" FORCE)
+    set(CMAKE_DEBUG_POSTFIX d CACHE INTERNAL "Use d as debug postfix for  targets" FORCE)
+
+    if (MSVC)
+      scryptAddCompilerFlags(/Gm- /MP /wd4251 /wd4244 /wd4307)## TODO test if DNOMINMAX can move here
+      ## Using MSVC STATIC RUNTIME)
+      ##foreach(flag_var CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+      ##  if(${flag_var} MATCHES "/MD")
+      ##    string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      ##  endif(${flag_var} MATCHES "/MD")
+      ##endforeach()
+    endif()
+
+    if(UNIX)
+      set(CMAKE_CXX_FLAGS "-fPIC -fvisibility=default" CACHE STRING "compilation flags for C++" FORCE)
+      set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3 " CACHE STRING "flags for C++ debug version" FORCE)
+      set(CMAKE_CXX_FLAGS_RELEASE "-O3 -g0 -DNDEBUG" CACHE STRING "flags for C++ release version" FORCE)
+      set(CMAKE_C_FLAGS "" CACHE STRING "flags for C" FORCE)
+      set(CMAKE_C_FLAGS_DEBUG "-O0 -g3" CACHE STRING "flags for C debug version" FORCE)
+      set(CMAKE_C_FLAGS_RELEASE "-O3 -g0 -DNDEBUG" CACHE STRING "flags for C release version" FORCE)
+    endif()
+
+    set(SCRYPT_SET_COMPILATION_OPTIONS_DONE TRUE CACHE BOOL "Protect to call twices")
+  endif()
+endmacro()
+
+#### Define output directories according to ${CMAKE_CONFIGURATION_TYPES} or ${CMAKE_BUILD_TYPE}
+#### Based on the defined 
+####   ${SCRYPT_SYSTEM_BUILD_ARCHI}
+####   ${CMAKE_CONFIGURATION_TYPES}
+####   ${CMAKE_BUILD_TYPE}
+function(scryptSetOutputDirectories)
+
+  if(SCRYPT_SET_OUTPUT_DIRS_DONE)
+    return()
+  else()
+    set(SCRYPT_SET_OUTPUT_DIRS_DONE TRUE CACHE BOOL "Protect to call twices")
+  endif()
+
+  if(NOT DEFINED SCRYPT_SYSTEM_BUILD_ARCHI)
+    message("  SetBuildFolders : variable [SCRYPT_SYSTEM_BUILD_ARCHI] is not defined")
+    message(FATAL_ERROR "On Windows, [SCRYPT_SYSTEM_BUILD_ARCHI] should be defined x64 or x86")
+  endif()
+
+  if(CMAKE_BUILD_TYPE)
+    set(_BuildTypeList "" "${CMAKE_BUILD_TYPE}")
+  else()
+    set(_BuildTypeList "${CMAKE_CONFIGURATION_TYPES}")
+  endif()
+
+  set(_LIST_OUTPUT_TYPES RUNTIME LIBRARY ARCHIVE)
+
+  foreach(_BuildType IN LISTS _BuildTypeList)
+    if(_BuildType)
+      string(TOUPPER ${_BuildType} _BUILD_TYPE)
+      string(TOLOWER ${_BuildType} _build_type_dir)
+      set(_BUILD_TYPE_TAG "_${_BUILD_TYPE}")
+    else()
+      set(_BUILD_TYPE_TAG "")
+      string(TOLOWER ${CMAKE_BUILD_TYPE} _build_type_dir)
+    endif()
+
+    set(_MAIN_OUTPUT_PATH "${CMAKE_BINARY_DIR}/x${SCRYPT_SYSTEM_BUILD_ARCHI}/${_build_type_dir}")
+    if(NOT EXISTS "${_MAIN_OUTPUT_PATH}")
+      file(MAKE_DIRECTORY "${_MAIN_OUTPUT_PATH}")
+    endif()
+    foreach(_OUT_TYPE ${_LIST_OUTPUT_TYPES})
+      set(CMAKE_${_OUT_TYPE}_OUTPUT_DIRECTORY${_BUILD_TYPE_TAG} "${_MAIN_OUTPUT_PATH}" CACHE PATH "Default path for runtime ouput directory" FORCE)
+      scryptAppendToGlobalSet(scryptOUTPUT_DIRECTORIES "CMAKE_${_OUT_TYPE}_OUTPUT_DIRECTORY${_BUILD_TYPE_TAG}=[${CMAKE_${_OUT_TYPE}_OUTPUT_DIRECTORY${_BUILD_TYPE_TAG}}]")
+    endforeach()
+  endforeach()
+
+  #scryptPrintList("scryptOUTPUT_DIRECTORIES" "${scryptOUTPUT_DIRECTORIES}")## Debug Log
+endfunction()
+
+function(scryptSetBuildVersion)
+  if(SCRYPT_SET_BUILD_VERSION_DONE)
+    return()
+  else()
+    set(SCRYPT_SET_BUILD_VERSION_DONE TRUE CACHE BOOL "Protect to call twices")
+  endif()
+
+  if(NOT DEFINED SCRYPT_VERSION_MAJOR)
+    set(SCRYPT_VERSION_MAJOR "1" CACHE INTERNAL "framework major version")
+  endif()
+  if(NOT DEFINED SCRYPT_VERSION_MINOR)
+    set(SCRYPT_VERSION_MINOR "0" CACHE INTERNAL "framework minor version")
+  endif()
+  if(NOT DEFINED SCRYPT_VERSION_PATCH)
+    set(SCRYPT_VERSION_PATCH "0" CACHE INTERNAL "framework patch version")
+  endif()
+  set(SCRYPT_VERSION_STRING "${SCRYPT_VERSION_MAJOR}.${SCRYPT_VERSION_MINOR}.${SCRYPT_VERSION_PATCH}" CACHE INTERNAL "scrypt version string")
+
+  # Get branch
+  scryptGetGitBranch(_SOURCE_GIT_COMMIT_BRANCH)
+  set(SOURCE_GIT_COMMIT_BRANCH ${_SOURCE_GIT_COMMIT_BRANCH} CACHE INTERNAL "Source commit hash")
+
+  scryptGetGitCommitHash(_SOURCE_GIT_COMMIT_HASH)
+  set(SOURCE_GIT_COMMIT_HASH ${_SOURCE_GIT_COMMIT_HASH} CACHE INTERNAL "Source commit hash")
+
+  # Get the latest commit datetime
+  scryptGetGitCommitDateTime(_SOURCE_GIT_COMMIT_DATETIME)
+  set(SOURCE_GIT_COMMIT_DATETIME ${_SOURCE_GIT_COMMIT_DATETIME} CACHE INTERNAL "Source commit datetime")
+
+  # Get the build date time
+  scryptGetBuildDateTime(_SCRYPT_BUILD_DATETIME_UTC)
+  set(SCRYPT_BUILD_DATETIME_UTC ${_SCRYPT_BUILD_DATETIME_UTC} CACHE INTERNAL "Build datetime UTC")
+
+  #### Generate version C++  #####
+  set(SCRYPT_VERSION_HPP_IN ${SCRYPT_ROOT_CMAKE_MODULE_PATH}/SCRYPTVersion.hpp.in CACHE INTERNAL "Template File for framework version config")
+  set(SCRYPT_VERSION_HPP ${SCRYPT_GENERATED_HPP_DIR}/SCRYPTVersion.hpp CACHE INTERNAL "HPP File for framework version config")
+  configure_file(${SCRYPT_VERSION_HPP_IN} ${SCRYPT_VERSION_HPP})
+endfunction()
---
+++ cmake/SCRYPTInit.cmake
@@ -0,0 +1,156 @@
+#################################################################
+#  Date             20/01/2020                                  #
+#  Author           Chi Thanh NGUYEN                            #
+#                                                               #
+#  Copyright (c) 2020 nChain Limited. All rights reserved       #
+#################################################################
+if(SCRYPTInit_Include) ## Include guard
+  return()
+endif()
+set(SCRYPTInit_Include TRUE)
+
+#####################################################
+#### Initialize everything related to cmake here ####
+#
+
+#### Hold the directory containing this current script ####
+set(SCRYPT_ROOT_CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}" CACHE PATH "Root directory of cmake modules")
+
+#### Set all directories/subdirectories in this root to CMAKE_MODULE_PATH
+macro(scryptInitModulePaths)
+  list(APPEND CMAKE_MODULE_PATH "${SCRYPT_ROOT_CMAKE_MODULE_PATH}")
+  scryptListSubDir(dirlist ${SCRYPT_ROOT_CMAKE_MODULE_PATH})
+  list(APPEND CMAKE_MODULE_PATH ${dirlist})
+endmacro()
+
+#### Force the CMAKE_BUILD_TYPE on UNIX like (not visual studio) system AND user doesn't specify the build type
+#### TODO add option build production vs dev. If prod --> default to release build, if dev --> default to debug build
+function(scryptSetCMakeBuildType)
+  if(NOT CMAKE_CONFIGURATION_TYPES AND NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE "Release" CACHE STRING "CMAKE_BUILD_TYPE default to Release on Unix system" FORCE)
+    message(WARNING "CMAKE_BUILD_TYPE was not defined, forced to [${CMAKE_BUILD_TYPE}]")
+  endif()
+endfunction()
+
+#### Set some technical value of the OS
+macro(scryptGetOSInfo)
+  ## Set x64/x86
+  if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(SCRYPT_SYSTEM_X64 ON)
+    set(SCRYPT_SYSTEM_BUILD_ARCHI 64 CACHE STRING "System archi")
+  else()
+    set(SCRYPT_SYSTEM_X86 ON)
+    set(SCRYPT_SYSTEM_BUILD_ARCHI 32 CACHE STRING "System archi")
+  endif()
+
+  ## SCRYPT_SYSTEM_OS used for package name.
+  ## Users can define -DCUSTOM_SYSTEM_OS_NAME=Ubuntu for example to have the customized os name on the installer
+  if(WIN32) #Windows
+    set(SCRYPT_SYSTEM_OS Windows CACHE STRING "System OS type")
+  else()    # UNIX
+    if(APPLE)
+        set(SCRYPT_SYSTEM_OS MacOS CACHE STRING "System OS type")
+    else()
+        set(SCRYPT_SYSTEM_OS Linux CACHE STRING "System OS type")
+    endif()
+  endif()
+endmacro()
+
+#### Create directory in binary tree to put all generated files
+macro(scryptCreateGeneratedDir)
+  set(SCRYPT_GENERATED_DIR "${CMAKE_BINARY_DIR}/generated" CACHE STRING "Directory containing all generated files")
+  file(MAKE_DIRECTORY "${SCRYPT_GENERATED_DIR}")
+endmacro()
+
+#### Create directory in binary tree to put all generated hpp files
+macro(scryptCreateGeneratedHppDir)
+  set(SCRYPT_GENERATED_HPP_DIR "${CMAKE_BINARY_DIR}/generated/hpp" CACHE STRING "Directory containing all generated hpp files")
+  file(MAKE_DIRECTORY "${SCRYPT_GENERATED_HPP_DIR}")
+  include_directories("${SCRYPT_GENERATED_HPP_DIR}")
+endmacro()
+
+#### Create directory in binary tree to put all generated tools files
+macro(scryptCreateGeneratedToolsDir)
+  set(SCRYPT_GENERATED_TOOLS_DIR "${CMAKE_BINARY_DIR}/generated/tools" CACHE STRING "Directory containing all generated tools")
+  file(MAKE_DIRECTORY "${SCRYPT_GENERATED_TOOLS_DIR}")
+endmacro()
+
+#### Check consistency of CMAKE_BUILD_TYPE vs CMAKE_CONFIGURATION_TYPES
+####   On build system like vs or xcode, CMAKE_CONFIGURATION_TYPES is defined for both Debug and Release
+####   On build system like Unix, CMAKE_BUILD_TYPE is defined indicating the build type
+####   Not both of them are defined
+macro(scryptTestBuildType)
+  ## Set x64/x86
+  if(CMAKE_BUILD_TYPE AND CMAKE_CONFIGURATION_TYPES)
+    message(FATAL_ERROR "Both CMAKE_BUILD_TYPE and CMAKE_CONFIGURATION_TYPES are defined, which is not expected")
+  endif()
+endmacro()
+
+#### Force CMAKE_INSTALL_PREFIX if not defined
+macro(scryptForceInstallDir)
+  set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/INSTALLATION" CACHE PATH "Cmake prefix" FORCE)
+  message(STATUS "SCRYPT WARNING: Forced CMAKE_INSTALL_PREFIX[${CMAKE_INSTALL_PREFIX}]")
+endmacro()
+
+#### Create API definition file
+function(scryptDefineDynamicAPI)
+  #Copy the API definition file
+  set(DYNAMIC_LIBRARY_API_HPP_IN ${SCRYPT_ROOT_CMAKE_MODULE_PATH}/DYNAMIC_LIBRARY_API.hpp.in)
+  set(DYNAMIC_LIBRARY_API_HPP ${SCRYPT_GENERATED_HPP_DIR}/DYNAMIC_LIBRARY_API.hpp)
+  configure_file(${DYNAMIC_LIBRARY_API_HPP_IN} ${DYNAMIC_LIBRARY_API_HPP})
+
+  include_directories("${SCRYPT_GENERATED_HPP_DIR}")
+  if(BUILD_ALL_STATIC_LIBS)
+    set(LIBS_BUILD_TYPE STATIC CACHE INTERNAL "Build type for all libraries")
+    add_definitions(-DBUILD_ALL_STATIC_LIBS)
+  else()
+    set(LIBS_BUILD_TYPE SHARED CACHE INTERNAL "Build type for all libraries")
+  endif()
+
+  message(STATUS "API definitions file created [${DYNAMIC_LIBRARY_API_HPP}]")
+  message(STATUS "  Use \${LIBS_BUILD_TYPE} instead of SHARED or STATIC in add_library for general propose")
+  message(STATUS "  Run cmake with option -DBUILD_ALL_STATIC_LIBS=ON to build all libraries in STATIC. Otherwise all libraries will be built in SHARED")
+  install(FILES "${DYNAMIC_LIBRARY_API_HPP}" DESTINATION "include" COMPONENT Files)
+endfunction()
+
+#### Initialize all setting for using CMake
+macro(scryptInitCMake)
+
+  scryptTestBuildType()
+  scryptSetCMakeBuildType()
+  scryptForceInstallDir()
+  cmake_policy(SET CMP0057 NEW)# USE IN_LIST functionality, example: if(${var} IN_LIST MYVAR_LIST)
+  cmake_policy(SET CMP0074 NEW)# Avoid warning when find_package
+  set_property(GLOBAL PROPERTY USE_FOLDERS ON)#
+  set_property(GLOBAL PROPERTY PREDEFINED_TARGETS_FOLDER "_CMakeTargets")
+
+  include(${SCRYPT_ROOT_CMAKE_MODULE_PATH}/SCRYPTTools.cmake)
+  scryptInitModulePaths()
+  #scryptPrintList("CMAKE_MODULE_PATH" "${CMAKE_MODULE_PATH}") ## Debug Log
+  scryptGetOSInfo()
+  scryptCreateGeneratedDir()
+  scryptCreateGeneratedHppDir()
+  scryptCreateGeneratedToolsDir()
+  #scryptPrintOSInfo()#Debug Log
+
+  include(SCRYPTBuildSetting)
+  scryptSetCompilationOptions()
+  scryptSetOutputDirectories()
+  scryptSetBuildVersion()
+  scryptDefineDynamicAPI()
+  install(FILES "${SCRYPT_VERSION_HPP}" DESTINATION "include" COMPONENT Files)
+
+  ## Precalculate variable for installation
+  scryptGetInstallRootDir(_install_root_dir)
+  set(SCRYPT_COMMON_INSTALL_PREFIX "${_install_root_dir}" CACHE PATH "Common directory used for installation")
+
+  include(FindOpenSSLHelper)
+  HelpFindOpenSSL()
+  #scryptPrintOpenSSLInfo()#Debug Log
+
+  include(FindBoostHelper)
+  HelpFindBoost()
+  #scryptPrintProperties(Boost::random)
+
+  enable_testing()
+endmacro()
