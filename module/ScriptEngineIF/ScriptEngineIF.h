ecaae7be141295912eb6e84739d65933b506981b
Chris Gibson <chris@gibsonic.com>
2020-06-17 11:55:15 +0000

    verifyScript parameter corrected to span

--- module/ScriptEngineIF/ScriptEngineIF.h
+++ module/ScriptEngineIF/ScriptEngineIF.h
@@ -4,15 +4,14 @@
 #include "span.h"
 
 #include <string>
-#include <memory>
 
 namespace ScriptEngineIF
 {
     bool executeScript(bsv::span<const uint8_t>);
-    bool verifyScript(const std::unique_ptr<unsigned char[]>&, const size_t& ); 
+    bool executeScript(const std::string&) ; 
     
-    bool executeScript(const std::string& ) ; 
-    bool verifyScript(const std::string& );
+    bool verifyScript(bsv::span<const uint8_t>); 
+    bool verifyScript(const std::string&);
 };
 
 #endif//#ifnde __SCRIPT_ENGINE_IF_H__
