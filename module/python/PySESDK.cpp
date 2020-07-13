438fd518de8ce4dfcf47162ae8b29a82761e09d1
Chris Gibson <chris@gibsonic.com>
2020-07-13 15:41:35 +0000

    replace NULL with nullptr

--- module/python/PySESDK.cpp
+++ module/python/PySESDK.cpp
@@ -28,24 +28,22 @@ static PyObject* wrap_ExecuteScript(PyObject* self, PyObject *args){
     int64_t amount(0); 
 
     if(!PyArg_ParseTuple(args,"siIsii", &scriptptr,&concensus, &scriptflags,&hextxptr,&nIndex,&amount))
-        return NULL;
+        return nullptr;
 
-    
     try{
         const ScriptError ret = bsv::evaluate(std::string{scriptptr},concensus, scriptflags, std::string{hextxptr},nIndex,amount);
         return Py_BuildValue("i", ret);
     }catch(std::exception& e){
         PyErr_SetString(PyExc_TypeError, e.what());
-        return(PyObject *) NULL;
+        return (PyObject*)nullptr;
     }
 }
 
-static PyMethodDef ModuleMethods[] =
-{
-    {"ExecuteScript", wrap_ExecuteScript,METH_VARARGS,"Execute a script"},
-    {NULL, NULL, 0, NULL},
+static PyMethodDef ModuleMethods[] = {
+    {"ExecuteScript", wrap_ExecuteScript, METH_VARARGS, "Execute a script"},
+    {nullptr, nullptr, 0, nullptr},
 };
- 
+
 #if PY_MAJOR_VERSION >= 3
 
 static int PySESDK_traverse(PyObject *m, visitproc visit, void *arg) {
@@ -58,20 +56,17 @@ static int PySESDK_clear(PyObject *m) {
     return 0;
 }
 
+static struct PyModuleDef moduledef = {PyModuleDef_HEAD_INIT,
+                                       "PySESDK",
+                                       nullptr,
+                                       sizeof(struct module_state),
+                                       ModuleMethods,
+                                       nullptr,
+                                       PySESDK_traverse,
+                                       PySESDK_clear,
+                                       nullptr};
 
-static struct PyModuleDef moduledef = {
-        PyModuleDef_HEAD_INIT,
-        "PySESDK",
-        NULL,
-        sizeof(struct module_state),
-        ModuleMethods,
-        NULL,
-        PySESDK_traverse,
-        PySESDK_clear,
-        NULL
-};
-
-#define INITERROR return NULL
+#define INITERROR return nullptr
 
 PyMODINIT_FUNC
 PyInit_PySESDK(void)
@@ -89,12 +84,13 @@ initmyextension(void)
     PyObject *module = Py_InitModule("PySESDK", myextension_methods);
 #endif
 
-    if (module == NULL)
+    if(module == nullptr)
         INITERROR;
     struct module_state *st = GETSTATE(module);
 
-    st->error = PyErr_NewException("PySESDK.Error", NULL, NULL);
-    if (st->error == NULL) {
+    st->error = PyErr_NewException("PySESDK.Error", nullptr, nullptr);
+    if(st->error == nullptr)
+    {
         Py_DECREF(module);
         INITERROR;
     }
