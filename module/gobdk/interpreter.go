07a8c06932a10aeea16b4226a1a3929fcafd1c1c
Chi Thanh NGUYEN <chithanhnguyen.math@gmail.com>
2024-08-13 15:34:28 +0200

    make the C++ built locally separated from the git source

--- module/gobdk/interpreter.go
+++ module/gobdk/interpreter.go
@@ -1,9 +1,23 @@
 package gobdk
 
+// To make this work on local dev
+//   Build the full sesdk library, make install it to a specific location. Then set the environment variables
+//
+//     export SESDK_ROOT=/path/to/install/directory
+//     export CGO_CFLAGS="-I${SESDK_ROOT}/include/cgo"
+//     export CGO_LDFLAGS="-L${SESDK_ROOT}/bin"
+//     export LD_LIBRARY_PATH=${SESDK_ROOT}/bin:$LD_LIBRARY_PATH
+//
+// To make a build inside docker, the same, i.e
+//   - Get the docker images that have all the necessary dependencies for C++ build
+//   - Build this sesdk libraries, and make install it to a location
+//   - Copy the installed files to the release image. To optimize, copy only the neccessary part,
+//   - For golang module, copy only the shared library go, and headers in cgo.
+// There might be other system shared library that is required for the executable, just copy them
+// to the release docker image. Use lld to know which one is missing.
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/cgo/include
-#cgo LDFLAGS: -L${SRCDIR}/cgo/bin -lGoSESDK -lstdc++ -lm -Wl,-rpath,./cgo/bin
+#cgo LDFLAGS: -lGoSESDK -lstdc++ -lm
 #include "interpreter_cgo.h"
 */
 import "C"
