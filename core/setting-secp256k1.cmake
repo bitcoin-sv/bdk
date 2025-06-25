8328d11faeab26d806cb7435bf66ad4f2a6061cc
Chi Thanh NGUYEN <5050018+ctnguyen@users.noreply.github.com>
2025-06-25 15:36:20 +0200

    remove hotfix for cmake in secp256k1 as it has been fixed in bsv

--- core/setting-secp256k1.cmake
+++ core/setting-secp256k1.cmake
@@ -23,23 +23,6 @@ if(MSVC)
   mark_as_advanced(CMAKE_MODULE_LINKER_FLAGS_COVERAGE)
 endif()
 
-
-## Backup CMAKE_C_FLAGS_RELEASE to do the hotfix
-set(BACKUP_CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
-
-## This hotfix because the code in ${BSV_ROOT}/src/secp256k1/CMakeLists.txt:175 produce a bug
-## It replace -O3 with all trailing whitespace by -O2. In the case the -O3 flags is at the begining
-## of the flag list, if will concatenate the -O2 with other flags, making if broken
-## We hotfix here by :
-##     Creating a temporary CMAKE_C_FLAGS_RELEASE specifically used for secp256k1
-##     Modify this temporary flags by putting the -O3 at the end of the list flags, so the bug
-##     doesn't have the change to produce
-##     Restore the CMAKE_C_FLAGS_RELEASE after the secp256k1 build
-if(NOT MSVC)
-  string(REPLACE "-O3" "" CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
-  set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3")
-endif()
-
 # We can disable the test building for secp256k1 here as it is not our job
 set(SECP256K1_BUILD_TESTS OFF)
 set(SECP256K1_BUILD_EXHAUSTIVE_TESTS OFF)
@@ -63,8 +46,6 @@ set(_BSV_SECP256K1_INSTALL_HDR_FILES
   "${BDK_BSV_ROOT_DIR}/src/secp256k1/include/secp256k1_extrakeys.h"
   "${BDK_BSV_ROOT_DIR}/src/secp256k1/include/secp256k1_recovery.h"
 )
-## Restore CMAKE_C_FLAGS_RELEASE after the hotfix for secp256k1 build
-set(CMAKE_C_FLAGS_RELEASE "{BACKUP_CMAKE_C_FLAGS_RELEASE}")
 
 #######################################################################################################################
 ## Install secp256k1 the *.h header files should be kept as secp256k1 structure    ####################################
